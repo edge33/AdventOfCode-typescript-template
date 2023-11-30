88313841bca020b1ab35c16d5f515ade9f4e65c4
Francesco <francesco@efem.dev>
2023-11-30 18:30:43 +0100

    Added bun, removed ts-node and nodemon

---
+++ src/days/2/Puzzle.ts
@@ -0,0 +1,13 @@
+const first = (input: string) => {
+  return 'solution 1';
+};
+
+const expectedFirstSolution = 'solution 1';
+
+const second = (input: string) => {
+  return 'solution 2';
+};
+
+const expectedSecondSolution = 'solution 2';
+
+export { first, expectedFirstSolution, second, expectedSecondSolution };
